0da435cfdb48f645c611175ed5a9b279eec648bc
rainforest0 <rain_forest_00@qq.com>
2022-01-12 22:14:18 +0800

    P106 106 Create the Power-up Class

---
+++ Source/CoopGame/Private/SpowerupActor.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpowerupActor.h"
+
+// Sets default values
+ASpowerupActor::ASpowerupActor()
+{
+	PowerupInterval = 0.0f;
+	TotalNumberOfTicks = 0;
+
+	bIsPowerupActive = false;
+
+	SetReplicates(true);
+}
+
+// Called when the game starts or when spawned
+void ASpowerupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASpowerupActor::OnTickPowerup()
+{
+	TicksProcessed++;
+
+	OnPowerupTicked();
+
+	if (TicksProcessed >= TotalNumberOfTicks)
+	{
+		OnExpired();
+
+		bIsPowerupActive = false;
+
+		// Delete timer
+		GetWorldTimerManager().ClearTimer(TimerHandle_PowerupTick);
+	}
+}
+
+
+void ASpowerupActor::ActivatePowerup()
+{
+	OnActivated();
+
+	bIsPowerupActive = true;
+
+	if (PowerupInterval > 0.0f)
+	{
+		GetWorldTimerManager().SetTimer(TimerHandle_PowerupTick, this, &ASpowerupActor::OnTickPowerup, PowerupInterval, true);
+	}
+	else
+	{
+		OnTickPowerup();
+	}
+	
+}
---
+++ Source/CoopGame/Public/SpowerupActor.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpowerupActor.generated.h"
+
+UCLASS()
+class COOPGAME_API ASpowerupActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpowerupActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Powerups")
+	float PowerupInterval;
+
+	//应用增强型道具效果的总次数
+	UPROPERTY(EditDefaultsOnly, Category = "Powerups")
+	int TotalNumberOfTicks;
+
+	FTimerHandle TimerHandle_PowerupTick;
+
+	// Total number of ticks applied
+	int32 TicksProcessed;
+
+	bool bIsPowerupActive;
+
+	UFUNCTION()
+	void OnTickPowerup();
+
+public:	
+	void ActivatePowerup();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Powerups")
+	void OnActivated();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Powerups")
+	void OnPowerupTicked();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Powerups")
+	void OnExpired();
+};
